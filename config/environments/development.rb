7720ae1beb3dfc3dc0328a2620d5eba631d35208
JohnnyFloyd <banan8p@tlen.pl>
2013-06-06 16:15:48 +0200

    Zmiany w dokumentacji

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,15 +7,16 @@ Projekt::Application.configure do
   config.cache_classes = false
   
   config.perform_caching = true
-  config.cache_store = :dalli_store, 'localhost:11211'
+  config.action_controller.perform_caching = true
+
+  config.cache_store = :dalli_store
+  #config.session_store :dalli_store, :memcache_server => ['host1', 'host2'], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 20.minutes
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
-
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
