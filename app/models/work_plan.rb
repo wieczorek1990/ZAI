b2d77156ffae9211c032a8f0880ff01118efd9b6
wieczorek1990 <wieczorek1990@gmail.com>
2013-07-14 18:40:05 +0200

    datefix

--- app/models/work_plan.rb
+++ app/models/work_plan.rb
@@ -9,12 +9,19 @@ class WorkPlan < ActiveRecord::Base
   validate :start_stop
   validate :bilocation
 
+  before_save :fix_date
+
   private
     def start_stop
       errors.add(:start, "sholudn't be greater or equal than #{:stop}") if self.start >= self.stop
     end
     
     def bilocation
-      errors.add(:doctor, "can't bilocate") if WorkPlan.joins(:clinic_doctor).where('doctor_id = ?', self.clinic_doctor.doctor_id).where('day = ?', self.day).where('work_plans.id != ?', self.id).where('(? BETWEEN start AND stop) OR (? BETWEEN start AND stop)', self.start, self.stop).exists?
+      errors.add(:doctor, "can't bilocate") if WorkPlan.joins(:clinic_doctor).where('doctor_id = ?', self.clinic_doctor.doctor_id).where('day = ?', self.day).where('work_plans.id != ?', self.id).where('(? BETWEEN start AND stop) OR (? BETWEEN start AND stop)', self.start , self.stop).exists?
+    end
+
+    def fix_date
+      self.start = 2000
+      self.stop = 2000
     end
 end
